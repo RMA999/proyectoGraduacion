93d99165dd793d710a49e8ecac748d69c63d665a
ronaldo <ronaldomorales13.rm@gmail.com>
2022-07-31 11:18:30 -0600

    moviendo archivos de lugar

---
+++ paginas/menu.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Document</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" 
+    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <nav>
+        <input type="checkbox" id ="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+
+    <ul class="menu">
+        <li><a href="#">Escanear</a></li>
+        <li><a href="#">Eliminar</a></li>
+        <li><a href="#">Modificar</a></li>
+        <li><a href="#">Buscar</a></li>
+    </ul>
+</nav>
+<section></section>
+</body>
+</html>
