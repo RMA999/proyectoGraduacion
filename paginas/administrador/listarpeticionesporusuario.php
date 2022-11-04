b301a95fd5bd95924bfb26e5e176392fee4082d5
exbinario <miguel.4to.1994@gmail.com>
2022-11-04 00:20:03 -0600

    separando lista de peticiones

--- paginas/administrador/listarpeticionesporusuario.php
+++ paginas/administrador/listarpeticionesporusuario.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tabla de usuarios</title>
+    <title>Peticiones</title>
 
     <?php
     include 'headers/headerBootstrap.php';
@@ -21,6 +21,12 @@
 
     <div class="container mt-2">
 
+        <?php if (isset($_GET['nombreUsuario'])) : ?>
+            <h2>Peticiones del usuario: <?php echo $_GET['nombreUsuario']; ?></h2>
+        <?php else : ?>
+            <h2>Mostrando todas las peticiones</h2>
+        <?php endif; ?>
+
         <table id="idTabladocumentos" class="table table-striped" width="100%"></table>
 
     </div>
@@ -33,28 +39,22 @@
                 'serverSide': true,
                 'serverMethod': 'post',
                 'ajax': {
-                    'url': '/funcionesphp/listarUsuarios.php'
+                    'url': '/funcionesphp/listarPeticionesAdmin.php',
+                    'data': {
+                        id_usuario: <?php echo $_GET['idUsuario']; ?>
+                    },
                 },
                 'columns': [{
                         title: '#',
                         "defaultContent": ""
                     },
                     {
-                        title: 'Dpi',
-                        data: 'dpi'
+                        title: 'No. Escritura',
+                        data: 'numero_escritura'
                     },
                     {
-                        title: 'Nombre',
-                        data: 'nombre'
-                    },
-                    {
-                        title: 'Usuario',
-                        data: 'nombre_usuario'
-                    },
-
-                    {
-                        title: 'Rol',
-                        data: 'nombre_rol'
+                        title: 'Tipo Documento',
+                        data: 'tipo_documento'
                     },
 
                     {
@@ -67,9 +67,8 @@
                         data: 'url_archivo',
                         "render": function(data, type, full) {
                             return '<div class="btn-group">' +
-                                '<button type="button" id="idAccionModificar" class="btn btn-outline-secondary" > <i class="fa-solid fa-pen-to-square" data-toggle="tooltip" data-placement="top" title="Modificar"></i> </button>' +
-                                '<button type="button" id="idAccionEliminar" class="btn btn-outline-secondary" > <i class="fa-solid fa-undo" data-toggle="tooltip" data-placement="top" title="Activar/Desactivar"></i> </button>' +
-                                '<button type="button" id="idAccionMostrarPeticiones" class="btn btn-outline-secondary" > <i class="fa-solid fa-list" data-toggle="tooltip" data-placement="top" title="Mostrar Peticiones"></i> </button>' +
+                                '<button type="button" id="idAccionAprobar" class="btn btn-outline-success" > <i class="fa-solid fa-check" data-toggle="tooltip" data-placement="top" title="Aprovar"></i> </button>' +
+                                '<button type="button" id="idAccionRechazar" class="btn btn-outline-danger" > <i class="fa-solid fa-x" data-toggle="tooltip" data-placement="top" title="Rechazar"></i> </button>' +
                                 '</div>'
                         },
                     }
@@ -106,56 +105,28 @@
 
             }).draw();
 
-            $('#idTabladocumentos tbody').on('click', '#idAccionMostrarPeticiones', function() {
-                var data = tabla.row($(this).parents('tr')).data();
-                console.log(data);
-                window.location.href = `/paginas/administrador/listarpeticionesporusuario.php?idUsuario=${data['id_usuario']}&&nombreUsuario=${data['nombre_usuario']}`;
-
-                // Swal.fire({
-                //     icon: 'info',
-                //     title: 'Funcion no disponible',
-                //     showConfirmButton: false
-                // });
-
-            });
-
-            $('#idTabladocumentos tbody').on('click', '#idAccionModificar', function() {
-                var data = tabla.row($(this).parents('tr')).data();
-                console.log(data);
-                window.location.href = `/paginas/administrador/modificarusuario.php?idUsuario=${data['id_usuario']}&&idPersona=${data['id_persona']}`;
-            });
-
-            $('#idTabladocumentos tbody').on('click', '#idAccionEliminar', function() {
-                var data = tabla.row($(this).parents('tr')).data();
-                console.log(data);
-
-                var mensaje = "";
-
-                if (data['estado'] === 'desactivado') {
-                    mensaje = `Activar el Usuario: ${data['nombre_usuario']}`
-                } else {
-                    mensaje = `Desactivar el Usuario: ${data['nombre_usuario']}`
-                }
-
-                if (data['nombre_rol'] == "Super Administrador") {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'No es permitido desactivar el usuario Super Administrador',
-                        showConfirmButton: false,
-                        showCloseButton: true,
-                    });
-                    return;
-                }
-
-                const usuarioAEliminar = {
-                    idUsuario: data['id_usuario'],
-                    idPersona: data['id_persona'],
-                    funcion: 'activarDesactivar'
+            function modificarPeticion(idPeticion, estado) {
+                const peticion = {
+                    id_peticion: idPeticion,
+                    estado,
+                    funcion: 'aprobarRechazar'
                 };
+                var mensaje = {
+                    pregunta: '',
+                    respuesta: ''
+                };
+                if (estado == 'Aprobada') {
+                    mensaje.pregunta = 'Esta seguro de aprobar esta petición';
+                    mensaje.respuesta = 'Petición Aprobada';
+                }
+                if (estado == 'Rechazada') {
+                    mensaje.pregunta = 'Esta seguro de rechazar esta petición';
+                    mensaje.respuesta = 'Petición Rechazada';
 
+                }
                 Swal.fire({
                     title: '¿Estas seguro?',
-                    text: mensaje,
+                    text: mensaje.pregunta,
                     icon: 'warning',
                     showCancelButton: true,
                     confirmButtonColor: '#3085d6',
@@ -166,7 +137,7 @@
                     if (result.isConfirmed) {
 
                         Swal.fire({
-                            title: 'Eliminando...',
+                            title: 'Procesando...',
                             timerProgressBar: true,
                             allowOutsideClick: false,
                             didOpen: () => {
@@ -176,35 +147,23 @@
 
                         $.ajax({
                             type: "POST",
-                            url: '/funcionesphp/funcionesUsuario.php',
-                            data: usuarioAEliminar,
+                            url: '/funcionesphp/peticiones.php',
+                            data: peticion,
                             success: function(response) {
                                 console.log(response);
 
-                                if (response.mensaje === "Usuario activado") {
+                                if (response.estado === "ok") {
 
                                     setTimeout(() => {
                                         Swal.fire({
                                             icon: 'success',
-                                            title: 'Usuario Activado',
+                                            title: mensaje.respuesta,
                                             showConfirmButton: false
                                         });
                                         tabla.ajax.reload();
                                     }, 1200);
-                                    return;
-                                }
 
-                                if (response.mensaje === "Usuario desactivado") {
 
-                                    setTimeout(() => {
-                                        Swal.fire({
-                                            icon: 'success',
-                                            title: 'Usuario Desactivado',
-                                            showConfirmButton: false
-                                        });
-                                        tabla.ajax.reload();
-                                    }, 1200);
-                                    return;
                                 }
 
 
@@ -214,7 +173,7 @@
                                 console.log(xhr, status);
                                 Swal.fire({
                                     icon: 'error',
-                                    title: 'Error al intentar eliminar el usuario',
+                                    title: 'Error al intentar modificar la petición',
                                     showConfirmButton: false,
                                     showCloseButton: true,
                                 });
@@ -224,7 +183,17 @@
 
                     }
                 });
+            }
 
+
+            $('#idTabladocumentos tbody').on('click', '#idAccionAprobar', function() {
+                var data = tabla.row($(this).parents('tr')).data();
+                modificarPeticion(data.id_peticion, 'Aprobada');
+            });
+
+            $('#idTabladocumentos tbody').on('click', '#idAccionRechazar', function() {
+                var data = tabla.row($(this).parents('tr')).data();
+                modificarPeticion(data.id_peticion, 'Rechazada');
             });
 
 
